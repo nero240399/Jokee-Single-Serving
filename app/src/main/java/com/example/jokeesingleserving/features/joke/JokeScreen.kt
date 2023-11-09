24b99e574b5402cec47aac2c79eb3c0ba54568e3
nero <hungnd.vnse@gmail.com>
2023-11-09 09:50:15 +0700

    [feat] implement UI component animations
    - Scrollable for joke content.
    - Expand option for copyright content.

--- app/src/main/java/com/example/jokeesingleserving/features/joke/JokeScreen.kt
+++ app/src/main/java/com/example/jokeesingleserving/features/joke/JokeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.jokeesingleserving.features.joke
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,19 +12,32 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -55,12 +71,14 @@ fun JokeScreen(
         TopBar(modifier = Modifier.padding(all = 16.dp))
         IntroductionBanner()
         Spacer(modifier = Modifier.height(64.dp))
-        JokeContent(uiState?.content ?: stringResource(id = R.string.out_of_joke))
-        Spacer(modifier = Modifier.weight(1f))
+        JokeContent(
+            content = uiState?.content ?: stringResource(id = R.string.out_of_joke),
+            modifier = Modifier.weight(1f)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         LikeButtons(feedbackJoke = feedbackJoke)
         Spacer(modifier = Modifier.height(16.dp))
-        Divider()
-        DisclaimerText(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp))
+        DisclaimerContent()
     }
 }
 
@@ -106,11 +124,15 @@ fun IntroductionBanner(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun JokeContent(content: String) {
+fun JokeContent(content: String, modifier: Modifier = Modifier) {
+    val state = rememberScrollState()
     Text(
         text = content,
-        modifier = Modifier.padding(horizontal = 32.dp),
-        style = MaterialTheme.typography.bodyLarge
+        style = MaterialTheme.typography.bodyLarge,
+        overflow = TextOverflow.Ellipsis,
+        modifier = modifier
+            .padding(horizontal = 32.dp)
+            .verticalScroll(state),
     )
 }
 
@@ -136,20 +158,42 @@ fun LikeButtons(
 }
 
 @Composable
-fun DisclaimerText(modifier: Modifier = Modifier) {
+fun DisclaimerContent(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    Box(contentAlignment = Alignment.Center) {
+        Divider()
+        IconButton(onClick = { expanded = !expanded }) {
+            Surface {
+                Icon(
+                    imageVector = if (expanded) Icons.Default.KeyboardArrowDown else Icons.Default.KeyboardArrowUp,
+                    contentDescription = null,
+                    modifier = Modifier.border(
+                        width = 1.dp,
+                        color = DividerDefaults.color,
+                        shape = CircleShape
+                    )
+                )
+            }
+        }
+    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
+            .padding(horizontal = 16.dp)
+            .animateContentSize()
     ) {
-        Text(
-            text = stringResource(id = R.string.disclaimer),
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-            style = MaterialTheme.typography.bodySmall
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+        if (expanded) {
+            Text(
+                text = stringResource(id = R.string.disclaimer),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                style = MaterialTheme.typography.bodySmall
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
         Text(
             text = stringResource(id = R.string.copyright),
             style = MaterialTheme.typography.titleSmall
         )
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
